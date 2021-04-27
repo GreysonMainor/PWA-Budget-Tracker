18b80d8d40a5a310c5f06724d8b4a4768d4a572e
Greyson Mainor <gbmainor98@outlook.com>
2021-04-27 18:25:03 -0400

    fixed my routes and connectiob.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -18,15 +19,16 @@ app.use(express.static("public"));
 //   useNewUrlParser: true,
 //   useFindAndModify: false
 // });
+app.use(require("./routes/api.js"));
 
- var MONGODB_URI = process.env.MONGODB_URI || "mongodb://dbmain:password1998@pwa-budget-tracker.yxzjk.mongodb.net/admin";
+ var MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://dbmain:password1998@pwa-budget-tracker.yxzjk.mongodb.net/test";
  mongoose.connect(MONGODB_URI,{  
      useNewUrlParser:true,
      useFindAndModify:false
  });
 
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
